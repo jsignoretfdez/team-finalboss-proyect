fe3430ad1392055b58f6e7c6ce574bf4dc338dd9
Jose Signoret <jsignoretfernandez@gmail.com>
2021-02-10 00:27:01 +0100

    Create models Users & Adverts

---
+++ backend/models/Adverts.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const advertsSchema = new Schema({
+
+    name: {
+        type: String,
+        required: true,
+    },
+
+    img: {
+        type: String,
+    },
+
+    description: {
+        type: String,
+        maxlength: 150
+    },
+
+    price: {
+        type: Number,
+        required: true,
+    },
+
+    type: {
+        type: String,
+        enum: ['sell', 'buy'],
+        required: true
+    },
+
+    tags: {
+        type: String,
+        enum: ['Tech', 'Sport', 'Games', 'Mobile', 'Toys']
+    },
+
+    user: {
+        type: Schema.Types.ObjectId, ref: 'Users'
+    },
+
+},{timestamps: true});
+
+const Adverts = mongoose.model('Adverts', advertsSchema);
+
+module.exports = Adverts;
---
+++ backend/models/Users.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const usuarioSchema = new Schema({
+
+    name:{
+        type: String,
+        required: true,
+    },
+
+    surname:{
+        type: String,
+    },
+
+    username:{
+      type:String,
+      unique:true,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    email:{
+        type:String,
+        unique:true,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+
+    password:{
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6
+    },
+
+    avatar:{
+        type: String,
+    },
+
+    favorites:[{type: Schema.ObjectId, ref: "Adverts"}]},
+
+    {
+        timestamps: true
+    }
+
+);
+
+usuarioSchema.statics.hashPassword = function (plainTextPass) {
+    return bcrypt.hash(plainTextPass, 12);
+};
+
+
+const Users = mongoose.model('Users', usuarioSchema);
+
+module.exports = Users;
