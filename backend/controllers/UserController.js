90cf002496a877b022abc08d9f515b05c81ae279
Jonathan Rod <jonyrm91@gmail.com>
2021-02-11 19:13:01 +0100

    eslint corrections

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,40 +1,35 @@
-//const mongoose = require('mongoose');
 const Users = require('../models/Users');
 
-
 exports.createUser = async (req, res, next) => {
-    const{name, surname, email, username,avatar} = req.body;
-    const passHash = await Users.hashPassword(req.body.password)
+  const { name, surname, email, username, avatar } = req.body;
+  const passHash = await Users.hashPassword(req.body.password);
 
-    try {
-        const userData = {
-            name,
-            username,
-            password: passHash,
-            email,
-            surname,
-            avatar
-        }
-
-        const user = new Users(userData);
-        await user.save()
-        console.log(user);
-        res.json(user);
-
-    } catch (err){
-        next(err);
-    }
-}
-
-exports.user = async (req, res, next) => {
-
-    const userDetail = await Users.findById(req.params.id);
-
-    if(!userDetail){
-        res.send(404).json({msg: "El usuario no existe"});
-        next();
+  try {
+    const userData = {
+      name,
+      username,
+      password: passHash,
+      email,
+      surname,
+      avatar,
     };
 
-    res.json(userDetail);
+    const user = new Users(userData);
+    await user.save();
+    console.log(user);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
 
-}
+exports.user = async (req, res, next) => {
+  const userDetail = await Users.findById(req.params.id);
+
+  if (!userDetail) {
+    res.send(404).json({ msg: 'El usuario no existe' });
+    next();
+  }
+
+  res.json(userDetail);
+};
