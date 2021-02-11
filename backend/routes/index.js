f82c9930656c005af7cebaf69c09167873d1f2e9
Jose Signoret <jsignoretfernandez@gmail.com>
2021-02-11 12:04:22 +0100

    Refactor route endpoints

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,16 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const AuthController = require('../controllers/authenticateController');
 
 module.exports = function () {
 
   /*Post create user*/
 
-  router.post('/registro', UserController.createUser);
+  router.post('/api/registro', UserController.createUser);
+  router.post('/api/login', AuthController.login);
 
   return router;
 
