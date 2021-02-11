8265290b6e89e1daf5d24b552d687f66fb0a6d74
Jose Signoret <jsignoretfernandez@gmail.com>
2021-02-11 14:54:29 +0100

    Add Function getUser

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -18,9 +18,23 @@ exports.createUser = async (req, res, next) => {
 
         const user = new Users(userData);
         await user.save()
+        console.log(user);
         res.json(user);
 
     } catch (err){
         next(err);
     }
+}
+
+exports.user = async (req, res, next) => {
+
+    const userDetail = await Users.findById(req.params.id);
+
+    if(!userDetail){
+        res.send(404).json({msg: "El usuario no existe"});
+        next();
+    };
+
+    res.json(userDetail);
+
 }
