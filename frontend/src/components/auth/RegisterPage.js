b00bc8a41ed048cec49c1e0b7638774e1d47dfa9
Kiara <kiaraymg@gmail.com>
2021-02-13 11:56:22 +0100

    Added RegisterPage.

--- frontend/src/components/auth/RegisterPage.js
+++ frontend/src/components/auth/RegisterPage.js
@@ -20,6 +20,11 @@ const RegisterPage = () => {
       <h2 className="register__title text-center my-4">Sign in!</h2>
       <form className="register__form" onSubmit={handleSubmit(onSubmit)} >
         <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="username" name="username" ref={register({ required: true })} />
+          {errors.username && <span className="error-message">This field is required</span>}
+        </div>
+        <div className="form-group">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" ref={register({ required: true })} />
           {errors.email && <span className="error-message">This field is required</span>}
@@ -28,10 +33,6 @@ const RegisterPage = () => {
           <label htmlFor="password">Password</label>
           <input type="password" name="password" ref={register({ required: true })} />
           {errors.password && <span className="error-message">This field is required</span>}
-        </div>
-        <div className="form-group d-flex align-items-center">
-          <label className="d-inline-block pr-3 mb-0" htmlFor="rememberMe">Remember me</label>
-          <input type="checkbox" name="rememberMe" ref={register} />
         </div>
         <button type="submit" className="btn btn-primary py-2 px-5">Send</button>
       </form>
