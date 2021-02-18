97cf27f06cfc7cf427ba639cb90516bf51ef345b
Jose Signoret <jsignoretfernandez@gmail.com>
2021-02-19 00:14:20 +0100

    Validation with Express-Validator of CRUD User

---
+++ backend/middleware/validateUser.js
@@ -0,0 +1,52 @@
+const { validationResult } = require('express-validator');
+const { check } = require('express-validator/check');
+const User = require('../models/Users');
+
+exports.validateUser = [
+  check('name')
+    .trim()
+    .toLowerCase()
+    .not()
+    .isEmpty()
+    .withMessage('El nombre es obligatorio.')
+    .bail(),
+  check('username')
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage('El nombre de usuario es obligatorio.')
+    .bail(),
+  check('email')
+    .trim()
+    .normalizeEmail()
+    .not()
+    .isEmpty()
+    .toLowerCase()
+    .withMessage('El email es obligatorio.')
+    .bail(),
+  check('password')
+    .not()
+    .isEmpty()
+    .isLength({ min: 6 })
+    .withMessage('La contraseña debe tener al menos 6 caracteres')
+    .bail(),
+  check('username').custom(async (username = '') => {
+    const usernameDuplicate = await User.findOne({ username });
+    if (usernameDuplicate) {
+      throw new Error('El nombre de usuario ya existe');
+    }
+  }),
+  check('email').custom(async (email = '') => {
+    const emailDuplicate = await User.findOne({ email });
+    if (emailDuplicate) {
+      throw new Error('El email existe en la base de datos');
+    }
+  }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
+    next();
+  },
+];
