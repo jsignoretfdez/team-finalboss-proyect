985b5314db0fa49bcbf23e99038987678e0484fd
Jose Signoret <jsignoretfernandez@gmail.com>
2021-02-19 00:23:39 +0100

    Create middleware in routes (auth,multer, validate)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,18 +1,30 @@
-'use strict';
-
 const express = require('express');
+
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 const AuthController = require('../controllers/authenticateController');
+const auth = require('../middleware/auth');
+const { upload } = require('../middleware/multer');
+const { validateUser } = require('../middleware/validateUser');
 
 module.exports = function () {
+  /* Routes CRUD User */
 
-  /*Post create user*/
-
-  router.post('/api/registro', UserController.createUser);
-  router.get('/api/cliente/:id', UserController.user);
+  router.post(
+    '/api/registro',
+    upload('avatar').single('avatar'),
+    validateUser,
+    UserController.createUser
+  );
+  router.get('/api/user/:id', auth, UserController.getUser);
+  router.put(
+    '/api/updateUser/:id',
+    auth,
+    validateUser,
+    UserController.updateUser
+  );
+  router.delete('/api/deleteUser/:id', auth, UserController.deleteUser);
   router.post('/api/login', AuthController.login);
 
   return router;
-
-}
+};
