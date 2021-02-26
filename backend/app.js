27be091f9153a3cec6217b2394c74e0a1cb1a758
Jonathan Rod <jonyrm91@gmail.com>
2021-02-26 19:09:21 +0100

    require user from app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/user');
 
 // Loading environmet variables
 require('dotenv').config();
